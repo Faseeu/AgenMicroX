a0e5447c9a0742f949b94a57ddb44063eabfba75
Faseeu <152055134+Faseeu@users.noreply.github.com>
2024-08-24 06:58:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from agent.planner_agent import PlannerAgent
 from agent.suggester_agent import SuggesterAgent
 from agent.browsing_agent import BrowsingAgent
 import os
-
+from openai import OpenAI
+from astra_assistants import patch
 
 
 def main():
     # Set the OpenAI key
-    openaikey=os.getenv('GROQ_API_KEY')
-    set_openai_key("openaikey")
+    client = patch(OpenAI())
+    set_openai_client(client)
 
     # Initialize agents
     planner = PlannerAgent()
@@ -21,13 +22,17 @@ def main():
 
     # Initialize the agency with the agent communication chart
     agency = Agency(
-        agency_chart=[
-            planner,                # Top-level agent
+        agency_chart=[senior_developer, #Top level agent
+            [senior_developer, planner],       
             [planner, suggester],   # Communication between planner and suggester
             [planner, browser],     # Communication between planner and browser
             [suggester, browser]    # Communication between suggester and browser
-        ]
+        ],
+        temperature=0.5
+        max_prompt_tokens=15000
     )
+    
+
 
     # Get user input
     user_input = input("Please provide your query: ")
